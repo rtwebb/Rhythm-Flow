f69192cd42799a6c559e8d47433977bd28127355
Robert <rtwebb@princeton.ed>
2022-04-26 13:20:28 -0400

    diffusion

--- COS-426-Final-Project/js/fluid.js
+++ COS-426-Final-Project/js/fluid.js
@@ -31,18 +31,20 @@ Fluid.advection = function(){
 // compute diffusion
 Fluid.diffusion = function(coords, ){
     // Grabbing coordinates
-    var x_left = new THREE.Vector2(Math.floor(coords.x - 1), Math.floor(coords.y));
+    var x_left = new THREE.Vector2(Math.floor(coords.x - 1), Math.floor(coords.y)); // should these be velocities or positions
     var x_right = new THREE.Vector2(Math.floor(coords.x + 1), Math.floor(coords.y));
     var x_top = new THREE.Vector2(Math.floor(coords.x), Math.floor(coords.y + 1));
     var x_bottom = new THREE.Vector2(Math.floor(coords.x), Math.floor(coords.y - 1));
 
-    var q_velocity = q[coords.x][coords.y].velocity; 
-    var alpha = Math.pow(q_velocity, 2) / time; // velocity is a 2d vector this will not work
-    var b_lower = q_velocity; 
-    var rbeta = 1 / (4 + Math.pow(q_velocity, 2)/time); // velocity is a 2d vector this will not work
-    var result = (x_left + x_right + x_top + x_bottom + alpha * b_lower) * rbeta;
+    var q_squared = q_velocity.clone().multiply(q_velocity);
 
-
+    var q_velocity = q[coords.x][coords.y].velocity.clone(); 
+    var alpha = q_squared.clone().divideScalar(time); // velocity^2 / time
+    var alpha_b_lower = q_velocity.clone().multiply(alpha);  //alpha * b
+    var val = q_squared.clone().divideScalar(time).addScalar(4); 
+    var rbeta = new THREE.Vector2(1/val.x, 1/val.y); // not sure if I am taking the reciprocal right
+    var result = (x_left.add(x_right).add(x_top).add(x_bottom).add(alpha_b_lower));
+    return result.multiply(rbeta);
 
 }
 
