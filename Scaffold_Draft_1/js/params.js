18c90248dbcac4e07a234cc1524554920017df86
Robert <rtwebb@princeton.ed>
2022-05-01 12:24:11 -0400

    naming

--- Scaffold_Draft_1/js/params.js
+++
@@ -1,9 +0,0 @@
-// This makes it so that constants are consistently defined across files. 
-class Params {
-    // Variables for simulation state; 
-    constructor() {
-      this.deltaT = 0.03;
-    }
-}
-
-var SceneParams = new Params();
