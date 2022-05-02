45a5957f7b601d2d07f5b92a271b17c57ce9d13a
Robert <rtwebb@princeton.ed>
2022-05-02 12:35:24 -0400

    wacky

--- Cos426 Final/script.js
+++ Cos426 Final/script.js
@@ -32,90 +32,22 @@ SOFTWARE.
 FREQUENCY ANALYSIS
 -----------------------------------------------------------------
 */
-//ctx.canvas.addEventListener('keydown', analyzeAudio);
-let ctx = canvas.getContext("2d");
-ctx.canvas.addEventListener('click', analyzeAudio);   
-function analyzeAudio(){
-    ctx.canvas.removeEventListener('click', analyzeAudio);
+function frequencyAnalyzer(){
+    // create audio context
+    let audioElement = document.getElementById("track");
+    const audioCtx = new AudioContext();
+    const analyser = audioCtx.createAnalyser();
     
+    // define size of array
+    analyser.fftSize = 2048;
+    let source = audioCtx.createMediaElementSource(audioElement);
     
-      // make a Web Audio Context
-  const context = new AudioContext();
-  const analyser = context.createAnalyser();
-
-  // Make a buffer to receive the audio data
-  const numPoints = analyser.frequencyBinCount;
-  const audioDataArray = new Uint8Array(numPoints);
-
-  function render() {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-    // get the current audio data
-    analyser.getByteFrequencyData(audioDataArray);
-
-    const width = ctx.canvas.width;
-    const height = ctx.canvas.height;
-    const size = 5;
-
-    // draw a point every size pixels
-    for (let x = 0; x < width; x += size) {
-      // compute the audio data for this point
-      const ndx = x * numPoints / width | 0;
-      // get the audio data and make it go from 0 to 1
-      const audioValue = audioDataArray[ndx] / 255;
-      // draw a rect size by size big
-      const y = audioValue * height;
-      ctx.fillRect(x, y, size, size);
-    }
-    requestAnimationFrame(render);
-  }
-  requestAnimationFrame(render);
-
-  // Make a audio node
-  const audio = new Audio();
-  audio.loop = true;
-  audio.autoplay = true;
-
-  // this line is only needed if the music you are trying to play is on a
-  // different server than the page trying to play it.
-  // It asks the server for permission to use the music. If the server says "no"
-  // then you will not be able to play the music
-  // Note if you are using music from the same domain 
-  // **YOU MUST REMOVE THIS LINE** or your server must give permission.
-  audio.crossOrigin = "anonymous";
-
-  // call `handleCanplay` when it music can be played
-  audio.addEventListener('canplay', handleCanplay);
-  audio.src = "https://twgljs.org/examples/sounds/DOCTOR%20VOX%20-%20Level%20Up.mp3";
-  audio.load();
-
-
-  function handleCanplay() {
-    // connect the audio element to the analyser node and the analyser node
-    // to the main Web Audio context
-    const source = context.createMediaElementSource(audio);
     source.connect(analyser);
-    analyser.connect(context.destination);
-  }
-
-
-    // // create audio context
-    // let audioElement = document.getElementById("track");
-    // const audioCtx = new AudioContext();
-    // const analyser = audioCtx.createAnalyser();
-    
-    // // define size of array
-    // analyser.fftSize = 2048;
-    // let source = audioCtx.createMediaElementSource(audioElement);
-    
-    // source.connect(analyser);
-    // source.connect(audioCtx.destination);
-    // let data = new Uint8Array(analyser.frequencyBinCount);
-    // analyser.getByteFrequencyData(data); //passing our Uint data array
+    source.connect(audioCtx.destination);
+    let data = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(data); //passing our Uint data array
 
 }
-
-
 
 /*
 -----------------------------------------------------------------
